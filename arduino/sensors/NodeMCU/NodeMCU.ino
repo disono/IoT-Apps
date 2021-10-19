39166e3bb713ec5ae8572d0defd48be2f8749cfb
Archie Disono <disono@users.noreply.github.com>
2021-10-19 10:18:39 +0800

    Update NodeMCU connection

--- arduino/sensors/NodeMCU/NodeMCU.ino
+++ arduino/sensors/NodeMCU/NodeMCU.ino
@@ -2,12 +2,65 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char* ssid = "";
+const char* password = "";
 
+//Your Domain name with URL path or IP address with path
+String serverName = "";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+// unsigned long timerDelay = 600000;
+// Set timer to 10 seconds (10000)
+unsigned long timerDelay = 10000;
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if ((millis() - lastTime) > timerDelay) {
+    // Check WiFi connection status
+    if(WiFi.status() == WL_CONNECTED) {
+      WiFiClient client;
+      HTTPClient http;
 
+      // path with lat, long and aclm data
+      String serverPath = serverName + "?lat=123&long=456&aclm=789";
+      http.begin(client, serverPath.c_str());
+
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+
+      if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        String payload = http.getString();
+        Serial.println(payload);
+      } else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+    
+    lastTime = millis();
+  }
 }
